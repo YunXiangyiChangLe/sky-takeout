277689350b56b6347492ebe913e66de29476e02b
shaozeguang <shaozeguang@tju.edu.cn>
2025-05-31 22:26:02 +0800

    购物车查看添加

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCardService.java
@@ -0,0 +1,20 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
+
+public interface ShoppingCardService {
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    public void addShoppingCard(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    List<ShoppingCart> showShoppingCard();
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCardServiceImpl.java
@@ -0,0 +1,71 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCardMapper;
+import com.sky.service.ShoppingCardService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Slf4j
+public class ShoppingCardServiceImpl implements ShoppingCardService {
+    @Autowired
+    private ShoppingCardMapper shoppingCardMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private SetmealMapper setmealMapper;
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    @Override
+    public void addShoppingCard(ShoppingCartDTO shoppingCartDTO) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+        List<ShoppingCart> list = shoppingCardMapper.list(shoppingCart);
+
+        if(list!=null && !list.isEmpty()){
+            ShoppingCart cart = list.get(0);
+            cart.setNumber(cart.getNumber()+1);
+            shoppingCardMapper.updateNumberById(cart);
+        }else {
+            Long dishId = shoppingCartDTO.getDishId();
+            Long setmealId = shoppingCartDTO.getSetmealId();
+            if(dishId!=null){
+                Dish dish=dishMapper.getById(dishId);
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+                shoppingCart.setNumber(1);
+                shoppingCart.setCreateTime(LocalDateTime.now());
+            }else{
+                log.error("暂时不能添加套餐");
+            }
+        }
+        shoppingCardMapper.insert(shoppingCart);
+    }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    @Override
+    public List<ShoppingCart> showShoppingCard() {
+        Long currentId = BaseContext.getCurrentId();
+        ShoppingCart list = ShoppingCart.builder().userId(currentId).build();
+        return shoppingCardMapper.list(list);
+    }
+}
